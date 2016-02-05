71bbbe778f9b38ac26d36405d7168ae8283029d9
subak <Subak Systems info@subak.jp>
2016-02-06 08:47:51 +0900

    client

---
+++ client/functions.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+docker17() {
+  cd
+  docker-compose run --rm docker17 docker "${@}"
+}
+
+docker18() {
+  cd
+  docker-compose run --rm docker18 docker "${@}"
+}
+
+docker19() {
+  cd
+  docker-compose run --rm docker19 docker "${@}"
+}
+
+docker110() {
+  cd
+  docker-compose run --rm docker110 docker "${@}"
+}
