6df0fb12a215db3d9d7ed19573bd79f43b63fdbc
subak <Subak Systems info@subak.jp>
2016-02-06 08:52:37 +0900

    oldpwd

--- client/functions.sh
+++ client/functions.sh
@@ -3,19 +3,23 @@
 docker17() {
   cd
   docker-compose run --rm docker17 docker "${@}"
+  cd ${OLDPWD}
 }
 
 docker18() {
   cd
   docker-compose run --rm docker18 docker "${@}"
+  cd ${OLDPWD}
 }
 
 docker19() {
   cd
   docker-compose run --rm docker19 docker "${@}"
+  cd ${OLDPWD}
 }
 
 docker110() {
   cd
   docker-compose run --rm docker110 docker "${@}"
+  cd ${OLDPWD}
 }
