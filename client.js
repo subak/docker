7738ea698276db72f0e40e804c6f74714ca13a23
subak <Subak Systems info@subak.jp>
2016-02-06 18:09:50 +0000

    cover windows

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 var WebSocket = require('ws'),
   ws = new WebSocket('ws://localhost:49153'),
-  spawn = require('child_process').spawn;
+  spawn = require('child_process').spawn,
+  os = require('os').platform();
 
 ws.on('open', function (m) {
   console.log(m);
@@ -9,7 +10,13 @@ ws.on('open', function (m) {
 ws.on('message', function (data) {
   var content = data.toString('utf8');
   console.log(content);
-  var pbcopy = spawn('pbcopy');
-  pbcopy.stdin.write(content);
-  pbcopy.stdin.end();
+  if (os.match(/^win/)) {
+    var clip = spawn('clip');
+    clip.stdin.write(content);
+    clip.stdin.end();
+  } else {
+    var pbcopy = spawn('pbcopy');
+    pbcopy.stdin.write(content);
+    pbcopy.stdin.end();
+  }
 });
