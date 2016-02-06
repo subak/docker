5297d17e1177109c41aeffa68b5f37fd3c0523f7
subak <Subak Systems info@subak.jp>
2016-02-06 18:18:56 +0000

    add stone

--- client/client-entrypoint.sh
+++ client/client-entrypoint.sh
@@ -13,4 +13,8 @@ git config --global user.email "${GIT_USER_EMAIL}"
 
 echo "${PROFILE}" > /root/.profile
 
+if [ -n "${STONE_OPTION}" ]; then
+  stone ${STONE_OPTION} &
+fi
+
 /usr/sbin/sshd "${@}"
