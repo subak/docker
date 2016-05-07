d07a1e2b863b27acc81768dbd8bba2c403035151
Subak Systems <subak@rmbp13.local>
2016-05-08 00:45:12 +0900

    日  5  8 00:45:12 JST 2016

--- client/client-entrypoint.sh
+++ client/client-entrypoint.sh
@@ -2,12 +2,6 @@
 
 set -eu
 
-echo "${SSH_ID_RSA}" > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-echo "${SSH_ID_RSA_PUB}" > /root/.ssh/id_rsa.pub
-echo "${SSH_ID_RSA_PUB}" > /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
-
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
 
