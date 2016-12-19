e0712b24499d448727c59b87aa129bf16023349e
Subak Systems <info@subak.jp>
2016-12-19 19:41:57 +0900

    2016年 12月19日 月曜日 19時41分57秒 JST

--- client/client-entrypoint.sh
+++ client/client-entrypoint.sh
@@ -7,6 +7,11 @@ git config --global user.email "${GIT_USER_EMAIL}"
 
 echo "${PROFILE}" > /root/.profile
 
+echo "${AUTHORIZED_KEYS}" > /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+
+echo "${SSH_ENVIRONMENT}" > /root/.ssh/environment
+
 if [ -n "${STONE_OPTION}" ]; then
   stone ${STONE_OPTION} &
 fi
